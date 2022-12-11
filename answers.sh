d3f0bbc8e4cb9e9475bfbbf83afb131405e648a5
zahraa mazloum <zahraamazloum2001@gmail.com>
2022-12-11 18:45:37 +0200

    Basic Queries

---
+++ answers.sh
@@ -0,0 +1,32 @@
+#BASIC QUERIES
+
+#1 
+"SELECT name from students"
+
+#2
+" select * from students
+ where age>30 "
+ 
+ #3
+ " select name from students 
+where Age='30'
+and Gender='F' "
+
+#4
+" Select points from students
+where name='Alex' "
+
+
+#5
+" INSERT into students(Name,Age,Gender,Points)
+Values('Zahraa','21','F',500) "
+
+#6 
+" update students
+set points=500
+where name='Basma' "
+
+#7
+"update students
+set points=150
+where name='Alex' "
