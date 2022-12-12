b78f71a4d75fd16615aded9d9567533ef44aa420
zahraa mazloum <zahraamazloum2001@gmail.com>
2022-12-12 18:53:20 +0200

    creating table

--- answers.sh
+++ answers.sh
@@ -30,3 +30,33 @@ where name='Basma' "
 "update students
 set points=150
 where name='Alex' "
+
+
+#Creating table
+
+#1
+" CREATE TABLE "graduates" ( 
+    "Id" INTEGER NOT NULL,
+    "Name" INTEGER NOT NULL UNIQUE,
+    "Age" INTEGER,
+    "Gender" TEXT, 
+    "Points" INTEGER, 
+    "Graduation" Date, 
+    PRIMARY KEY("Id" AUTOINCREMENT) ); "
+    
+
+
+" insert into graduates(Name,Age,Gender,Points)
+select name,age,gender,points
+FROM students
+where students.name='Layal'
+"
+
+#2
+" update graduates 
+set Graduation=('08/09/2018')
+WHERE Name='Layal'; "
+
+#3
+" DELETE  from students
+where name='Layal' "
